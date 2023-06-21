1d21f96702a0b86351fbc5b1797b0da44a212f38
Umuhasna04 <fa1033170@gmail.com>
2023-06-21 15:39:11 -0700

    Fix add only the even values

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,13 +11,14 @@ int main(void)
 	unsigned long int i = 1, j = 2;
 	unsigned long int sum = 0, result = 0;
 
-	result = i + j;
+	result = j;
 	while (sum < 4000000)
 	{
 		sum = i + j;
 		i = j;
 		j = sum;
-		result += sum;
+		if (sum % 2 == 0)
+			result += sum;
 	}
 	printf("%ld\n", result);
 
