2616f07fdcd4cbf0d1135ef844e8be5ec10f3fc2
Umuhasna04 <fa1033170@gmail.com>
2023-07-07 02:55:02 -0700

    Fix betty documentation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,8 @@
  * main - program that prints its name
  * @argc: number of arguments
  * @argv: array containing the program command line arguments
+ *
+ * Return: always 0
  */
 int main(int __attribute__((unused))argc, char *argv[])
 {
