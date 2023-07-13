5c809afb544ff6325eca759eca87cb8d42f5bdd0
Umuhasna04 <fa1033170@gmail.com>
2023-07-13 07:03:49 -0700

    Fix remove 1 byte from memory to allocate by malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!nmemb || !size)
 		return (NULL);
 
-	ptr = malloc(1 + nmemb * size);
+	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
 
