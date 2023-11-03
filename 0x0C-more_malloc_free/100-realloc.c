f67bd692a903ac29b76d96b1b532cb7e529e454e
Umuhasna04 <fa1033170@gmail.com>
2023-11-03 08:46:30 -0700

    Copy the content from the old memory to the new

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr;
+	void *new_ptr;
 
 	if (!ptr)
 	{
@@ -30,11 +30,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 	{
-		new_ptr = malloc(old_size + new_size);
+		new_ptr = malloc(new_size);
 		if (!new_ptr)
 			return (NULL);
 
-		strncpy(new_ptr, ptr, old_size);
+		strncpy(new_ptr, ptr, new_size);
+
+		((char *)new_ptr)[new_size] = '\0';
 
 		free(ptr);
 		return (new_ptr);
